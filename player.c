789c654f9e1b2bf1ca5311441c2e437c0fb96402
Georgia A. Mandell <f0044sn@plank.thayer.dartmouth.edu>
2022-02-28 09:50:34 -0500

    added .c and .h files for player

---
+++ player.c
@@ -0,0 +1,7 @@
+/* 
+ * player.c - Nuggets 'player' module
+ * 
+ * see player.h for more information
+ * 
+ * CS50, Winter 2022 
+ */
---
+++ player.h
@@ -0,0 +1,9 @@
+/* 
+ * player.h - header file for Nuggets 'player' module
+ * 
+ * A 'player' is a module providing the data structure to represent a player in the Nuggets game. 
+ * Each player keeps track of its own name, location, the amount of gold it has, 
+ * the rooms it has seen before (its personal grid struct), and whether the player is currently talking to the server.
+ *
+ * CS50, Winter 2022 
+ */
