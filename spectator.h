e1223c702d8de0491b0b9805c3d5dc993354aa10
Jefferson Mubarak Adjetey <f004pkp@plank.thayer.dartmouth.edu>
2022-02-28 09:53:29 -0500

    complete spectator

---
+++ spectator.h
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Message module
+#include "message.h" 
+
+
+
+//spectator struct
+typedef struct spectator {
+  addr_t address;
+} spectator_t;
+
+
+
+
+
+
