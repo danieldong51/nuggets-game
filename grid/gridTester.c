d19d8e5634388ea7cf87642f61f1713e11f71bb8
Dan Dong <danieldong51@gmail.com>
2022-03-04 16:38:03 -0500

    updated gridTester

--- grid/gridTester.c
+++ grid/gridTester.c
@@ -15,16 +15,24 @@ int
 main(const int argc, char* argv[])
 {
 
+  printf("line start\n");
+
   grid_t* masterGrid = grid_new();
   //printf("made grid\n");
   char* fileName; 
   fileName = argv[1];
+
+  printf("line 1\n");
+
   //FILE* fp = fopen(fileName, "r");
   //char line[15];
   //fgets(line, 15, fp);
   //printf("tester line: %s\n", line);
   gridMakeMaster(masterGrid, fileName, 10, 1 ,3, 1);
   //printf("made master\n");
+
+  printf("line 29\n");
+
   char** grid2D = getGrid2D(masterGrid);
   int NR = getNumRows(masterGrid);
 
@@ -33,16 +41,15 @@ main(const int argc, char* argv[])
     printf("%s\n", grid2D[i]);
   }
 
+  printf("line 36\n");
+
   // Reads main.txt mao
   FILE* mapFile = fopen("../maps/main.txt", "r");
-  gridConvert(grid2D, mapFile, getNumRows(masterGrid), getNumColumns(masterGrid));
+  gridConvert(grid2D, mapFile, getNumRows(masterGrid), getNumColumns(masterGrid));  
 
-  // Creating new player
-  player_t* playerA = player_new("tempName", "a", masterGrid);
+  printf("line 42\n");
 
   gridPrint(masterGrid, 'a');
-  
-
 
   
 }
