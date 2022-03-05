f0a242b6c60def85169b7c964e3e3ace61cf8252
Georgia Mandell <georgia.a.mandell.23@dartmouth.edu>
2022-03-05 17:01:24 -0500

    spectator changes

--- spectator/spectator.h
+++ spectator/spectator.h
@@ -20,20 +20,23 @@ typedef struct spectator spectator_t;
 /**************** local functions ****************/
 spectator_t* spectator_new(grid_t* masterGrid, addr_t address);
 void spectator_delete(spectator_t* spectator);
+grid_t* spectator_getGrid(spectator_t* spectator);
+addr_t* spectator_getAddress(spectator_t* spectator);
 
-//getter
-addr_t* getAddress(spectator_t* spectator);
 
-
-/************spectator_new***********/
+/************ spectator_new ***********/
 spectator_t* spectator_new(grid_t* masterGrid, addr_t address);
 
 
-/**********spectator_delete**********/
+/********** spectator_delete **********/
 void spectator_delete(spectator_t* spectator);
 
 
+/************ spectator_getAddress ***********/
+addr_t* spectator_getAddress(spectator_t* spectator);
 
+/******** spectator_getGrid ************/
+grid_t* spectator_getGrid(spectator_t* spectator);
 
 
 
